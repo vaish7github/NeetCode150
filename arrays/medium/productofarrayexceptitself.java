51bb1414d91cfe5b535ad05833291fcb71f7c5d2
vaish7github <vaishnavitiwari968@gmail.com>
2025-08-15 16:56:11 +0530

    product of array except itself

---
+++ arrays/medium/productofarrayexceptitself.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class main{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        int [] nums= new int[n];
+        for (int i=0;i< nums.length ;i++){
+            nums[i]= sc.nextInt();
+        }
+        sc.close();
+        int[] ans = productExceptSelf(nums);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    public static int[] productExceptSelf(int [] nums){
+        int n= nums.length;
+        int [] left = new int[n];
+        left[0]= 1;
+        for (int i =1;i< nums.length;i++){
+            left[i]= nums[i-1]*left[i-1];
+        }
+        int [] right = new int[n];
+        right[n-1]=1;
+        for (int i=n-2;i>-1;i--){
+            right[i]= right[i+1]*nums[i+1];
+        } 
+        int [] ans = new int[n];
+        for (int i =0;i< nums.length;i++){
+            ans[i]= right[i]*left[i];
+        }
+
+        return ans;
+    }
+}
